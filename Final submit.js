b5bd33a856c332c94c0e9b6ab6f9154a50bace26
nazmul-islam1412 <mohammad.nazmul.islam1412@gmail.com>
2025-08-04 02:28:18 +0600

    added problem no.5

--- Final submit.js
+++ Final submit.js
@@ -45,3 +45,9 @@ else if(fileName.endsWith(".docx")){
 else{
     console.log("Delete");
 }
+
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhankar" , roll: 1014 ,department: "cse" };
+//write your code here
+console.log(student.name+student.roll+"."+student.department+"@ph.ac.bd");
+
---
+++ problem5.js
@@ -0,0 +1,4 @@
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhankar" , roll: 1014 ,department: "cse" };
+//write your code here
+console.log(student.name+student.roll+"."+student.department+"@ph.ac.bd");
