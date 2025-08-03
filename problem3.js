9c4a94c1f06125f3c3c2c3f56e783cddd61a64fc
nazmul-islam1412 <mohammad.nazmul.islam1412@gmail.com>
2025-08-04 00:54:29 +0600

    added problem no.3

---
+++ problem3.js
@@ -0,0 +1,12 @@
+var lastDay=4;
+
+for(i=1;i<=lastDay;i++){
+    if(i%3==0){
+        console.log(i+" "+"- medicine");
+    }
+    else{
+        console.log(i+" "+"- rest");
+    }
+}
+
+//1 - rest
