b548612b55ff6b0e26e551824f5ea904d51000c2
nazmul-islam1412 <mohammad.nazmul.islam1412@gmail.com>
2025-08-04 00:33:12 +0600

    New commit message

---
+++ Final submit.js
@@ -0,0 +1,20 @@
+//Problem NO.1
+var area=100;
+
+var shareForBoth=area/2;
+
+console.log(shareForBoth);
+
+//Problem NO. 2
+
+var money=30000;
+
+if(money>=25000){
+    console.log("Laptop");
+}
+else if(money>=10000 && money<25000){
+    console.log("Cycle");
+}
+else{
+    console.log("Chocolate");
+}
---
+++ problem2.js
@@ -0,0 +1,11 @@
+var money=30000;
+
+if(money>=25000){
+    console.log("Laptop");
+}
+else if(money>=10000 && money<25000){
+    console.log("Cycle");
+}
+else{
+    console.log("Chocolate");
+}
